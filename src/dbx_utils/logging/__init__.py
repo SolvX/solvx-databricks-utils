4bdcb13d0550c65846b8cdfed5431734ec663eef
Vladislav Jansky <v.jansky@outlook.com>
2025-11-27 08:53:49 +0100

    Squashed commit of the following:
    commit 169eecff2fc7521a01ef72d0b16701bfb81f8956
    Author: Vladislav Jansky <v.jansky@outlook.com>
    Date:   Thu Nov 27 08:53:14 2025 +0100
    
        added some
    
    commit c1c0752688b668e7d19a54bf54372845e0d14865
    Author: Vladislav Jansky <67694456+VladislavJansky@users.noreply.github.com>
    Date:   Tue Nov 25 17:20:38 2025 +0100
    
        Fix repo root detection in debug notebook
    
    commit 503de4cf1b08fe209bfb65c8a24745875f23681f
    Author: Vladislav Jansky <67694456+VladislavJansky@users.noreply.github.com>
    Date:   Tue Nov 25 17:14:59 2025 +0100
    
        Improve debug notebook default path
    
    commit b8d4b94490ecf8020131143357a6c6dd72ada96c
    Author: VladislavJansky <V.Jansky@outlook.com>
    Date:   Tue Nov 25 16:08:12 2025 +0000
    
        added debug
    
    commit 22b28930279d5530821fb1d998a0918144c34c2b
    Author: VladislavJansky <V.Jansky@outlook.com>
    Date:   Tue Nov 25 14:36:37 2025 +0000
    
        added logging

---
+++ src/dbx_utils/logging/__init__.py
@@ -0,0 +1,34 @@
+"""
+Databricks-friendly logger built on the standard :mod:`logging` package.
+"""
+
+import logging
+import sys
+from typing import Optional
+
+DEFAULT_LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+
+def getLogger(name: Optional[str] = None, level: int = logging.INFO) -> logging.Logger:
+    """
+    Return a pre-configured logger.
+
+    level can be set by:
+    import logging
+    level = logging.INFO
+    or the representing INT
+    """
+    logger_name = name or __name__
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(level)
+
+    if not any(isinstance(handler, logging.StreamHandler) for handler in logger.handlers):
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(level)
+        handler.setFormatter(logging.Formatter(DEFAULT_LOG_FORMAT))
+        logger.addHandler(handler)
+
+    return logger
+
+
+__all__ = ["getLogger", "DEFAULT_LOG_FORMAT"]
---
+++ tests/test_logger.py
@@ -0,0 +1,27 @@
+from dbx_utils import DEFAULT_LOG_FORMAT, getLogger
+
+
+def test_get_logger_writes_to_stdout(capsys):
+    logger = getLogger("sample_logger")
+    logger.info("hello from logger")
+
+    captured = capsys.readouterr().out
+    assert "sample_logger" in captured
+    assert "hello from logger" in captured
+
+
+def test_get_logger_sets_default_format():
+    logger = getlogger("format_logger", level=logging.DEBUG)
+    handler = next(
+        h for h in logger.handlers if isinstance(h, logging.StreamHandler)
+    )
+    assert handler.formatter._fmt == DEFAULT_LOG_FORMAT
+
+
+def test_get_logger_is_idempotent():
+    logger = getLogger("idempotent")
+    initial_handlers = list(logger.handlers)
+
+    logger_again = getLogger("idempotent")
+    assert logger_again is logger
+    assert logger_again.handlers == initial_handlers
